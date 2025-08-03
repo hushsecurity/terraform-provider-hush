8b131df79c596cf2019304926c208c66811630ba
Omri Zilberstein <omri@hush.security>
2025-08-03 18:42:43 +0300

    HUSH-2233 client, provider: Support deployments

---
+++ internal/client/deployment.go
@@ -0,0 +1,106 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+const deploymentsEndpoint = "/v1/deployments"
+
+type Deployment struct {
+	ID          string `json:"id,omitempty"`
+	Name        string `json:"name"`
+	Description string `json:"description,omitempty"`
+	EnvType     string `json:"env_type"`
+	Status      string `json:"status,omitempty"`
+	CreatedAt   string `json:"created_at,omitempty"`
+	ModifiedAt  string `json:"modified_at,omitempty"`
+}
+
+// CreateDeploymentInput represents the input for creating a deployment
+type CreateDeploymentInput struct {
+	Name        string `json:"name"`
+	Description string `json:"description,omitempty"`
+	EnvType     string `json:"env_type"`
+}
+
+// UpdateDeploymentInput represents the input for updating a deployment
+type UpdateDeploymentInput struct {
+	Name        *string `json:"name,omitempty"`
+	Description *string `json:"description,omitempty"`
+	EnvType     *string `json:"env_type,omitempty"`
+}
+
+// DeploymentCredentialsResponse embeds Deployment and adds credentials
+type DeploymentCredentialsResponse struct {
+	Deployment
+	Token           string `json:"token"`
+	Password        string `json:"password"`
+	ImagePullSecret string `json:"image_pull_secret"`
+}
+
+func CreateDeployment(ctx context.Context, c *Client, input *CreateDeploymentInput) (*Deployment, error) {
+	var resp DeploymentCredentialsResponse
+	if err := c.doRequest(ctx, http.MethodPost, deploymentsEndpoint, input, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Deployment, nil
+}
+
+// CreateDeploymentWithCredentials creates a deployment and returns the full credentials response
+func CreateDeploymentWithCredentials(ctx context.Context, c *Client, input *CreateDeploymentInput) (*DeploymentCredentialsResponse, error) {
+	var resp DeploymentCredentialsResponse
+	if err := c.doRequest(ctx, http.MethodPost, deploymentsEndpoint, input, &resp); err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
+
+func GetDeployment(ctx context.Context, c *Client, id string) (*Deployment, error) {
+	path := fmt.Sprintf("%s/%s", deploymentsEndpoint, id)
+	var dep Deployment
+	err := c.doRequest(ctx, http.MethodGet, path, nil, &dep)
+	if err != nil {
+		if err.Error() == "unexpected status: 404" {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &dep, nil
+}
+
+func UpdateDeployment(ctx context.Context, c *Client, id string, input *UpdateDeploymentInput) (*Deployment, error) {
+	path := fmt.Sprintf("%s/%s", deploymentsEndpoint, id)
+
+	var result Deployment
+	if err := c.doRequest(ctx, http.MethodPatch, path, input, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func DeleteDeployment(ctx context.Context, c *Client, id string) error {
+	tflog.Info(ctx, "Deleting deployment", map[string]interface{}{
+		"deployment_id": id,
+	})
+
+	path := fmt.Sprintf("%s/%s", deploymentsEndpoint, id)
+
+	err := c.doRequest(ctx, http.MethodDelete, path, nil, nil)
+	if err != nil {
+		tflog.Error(ctx, "Failed to delete deployment", map[string]interface{}{
+			"deployment_id": id,
+			"error":         err.Error(),
+		})
+		return err
+	}
+
+	tflog.Info(ctx, "Successfully deleted deployment", map[string]interface{}{
+		"deployment_id": id,
+	})
+
+	return nil
+}
---
+++ internal/provider/deployment/resource.go
@@ -0,0 +1,131 @@
+package deployment
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hushsecurity/terraform-provider-hush/internal/client"
+)
+
+const resourceDescription = "Deployment resource for managing Hush Security deployments"
+
+func Resource() *schema.Resource {
+	return &schema.Resource{
+		Description: resourceDescription,
+
+		CreateContext: deploymentCreate,
+		ReadContext:   deploymentRead,
+		UpdateContext: deploymentUpdate,
+		DeleteContext: deploymentDelete,
+		Schema:        DeploymentResourceSchema(),
+	}
+}
+
+func deploymentCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	input := &client.CreateDeploymentInput{
+		Name:        d.Get("name").(string),
+		Description: d.Get("description").(string),
+		EnvType:     d.Get("env_type").(string),
+	}
+
+	resp, err := client.CreateDeploymentWithCredentials(ctx, c, input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	tflog.Debug(ctx, "Created deployment", map[string]interface{}{
+		"deployment_id": resp.ID,
+		"name":          resp.Name,
+	})
+
+	if resp.ID == "" {
+		return diag.Errorf("API returned empty ID for new deployment")
+	}
+
+	d.SetId(resp.ID)
+
+	if diags := setCredentialFields(d, resp); diags.HasError() {
+		return diags
+	}
+
+	if diags := setDeploymentFields(d, &resp.Deployment); diags.HasError() {
+		return diags
+	}
+
+	return nil
+}
+
+func deploymentUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	input := &client.UpdateDeploymentInput{}
+	hasChanges := false
+
+	if d.HasChange("description") {
+		desc := d.Get("description").(string)
+		input.Description = &desc
+		hasChanges = true
+	}
+	if d.HasChange("env_type") {
+		envType := d.Get("env_type").(string)
+		input.EnvType = &envType
+		hasChanges = true
+	}
+	if d.HasChange("name") {
+		name := d.Get("name").(string)
+		input.Name = &name
+		hasChanges = true
+	}
+
+	if !hasChanges {
+		return nil
+	}
+
+	updated, err := client.UpdateDeployment(ctx, c, d.Id(), input)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to update deployment: %w", err))
+	}
+
+	tflog.Debug(ctx, "Updated deployment", map[string]interface{}{
+		"deployment_id": d.Id(),
+	})
+
+	if diags := setDeploymentFields(d, updated); diags.HasError() {
+		return diags
+	}
+
+	return nil
+}
+
+func deploymentDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*client.Client)
+
+	err := client.DeleteDeployment(ctx, c, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func setCredentialFields(d *schema.ResourceData, resp *client.DeploymentCredentialsResponse) diag.Diagnostics {
+	credentials := map[string]interface{}{
+		"token":             resp.Token,
+		"password":          resp.Password,
+		"image_pull_secret": resp.ImagePullSecret,
+	}
+
+	for field, value := range credentials {
+		if err := d.Set(field, value); err != nil {
+			return diag.FromErr(fmt.Errorf("failed to set %s: %w", field, err))
+		}
+	}
+
+	return nil
+}
